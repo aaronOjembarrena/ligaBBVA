fbe8adf7f9db5cdf38eccb1f7e8c2513a8f3c28b
Aaron <Aaron@Aaron-PC>
2014-01-29 16:25:41 +0100

    Comenzado proyecto contador de piezas

---
+++ cuentaPower2000/src/cuentapower2000/EstructuraDatos.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentapower2000;
+
+/**
+ *
+ * @author Aaron
+ */
+public class EstructuraDatos {
+    
+}
